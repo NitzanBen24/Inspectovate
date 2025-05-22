e5b331fdc4c94bc1e35e63b4cfd9787d3a0a17d0
NitzanBen24 <nitzanben24@gmail.com>
2025-05-22 22:05:31 +0300

    V3: new split prod and local

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,26 +1,26 @@
-import chromium from '@sparticuz/chromium';
+// import chromium from '@sparticuz/chromium';
 
-export async function launchBrowser() {
-  const isProduction = process.env.VERCEL === '1';
+// export async function launchBrowser() {
+//   const isProduction = process.env.VERCEL === '1';
 
-  if (isProduction) {
-    // @sparticuz/chromium handles extraction internally and returns correct path
-    const executablePath = await chromium.executablePath();
+//   if (isProduction) {
+//     // @sparticuz/chromium handles extraction internally and returns correct path
+//     const executablePath = await chromium.executablePath();
 
-    return await import('puppeteer-core').then(({ default: puppeteerCore }) =>
-      puppeteerCore.launch({
-        args: chromium.args,
-        executablePath,
-        headless: chromium.headless,
-      })
-    );
-  } else {
-    // Use full puppeteer for local development
-    return await import('puppeteer').then(({ default: puppeteer }) =>
-      puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      })
-    );
-  }
-}
+//     return await import('puppeteer-core').then(({ default: puppeteerCore }) =>
+//       puppeteerCore.launch({
+//         args: chromium.args,
+//         executablePath,
+//         //headless: chromium.headless,
+//       })
+//     );
+//   } else {
+//     // Use full puppeteer for local development
+//     return await import('puppeteer').then(({ default: puppeteer }) =>
+//       puppeteer.launch({
+//         headless: true,
+//         args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//       })
+//     );
+//   }
+// }
