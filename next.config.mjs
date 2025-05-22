24ac9c23f5e1173fc7ae1bb5118e9a03f65faef6
NitzanBen24 <nitzanben24@gmail.com>
2025-05-22 18:54:41 +0300

    V3: fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,7 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack(config) {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.join(process.cwd(), 'node_modules', '@sparticuz', 'chromium', 'bin'),
-            to: path.join(process.cwd(), '.next', 'server', 'bin'),
-          },
-        ],
-      })
-    );
-
+  webpack(config) {        
     config.resolve.alias['@fortawesome/fontawesome-svg-core/styles.css'] =
       '@fortawesome/fontawesome-svg-core';
 
--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,26 +1,18 @@
-import path from 'path';
-import puppeteer, { Browser } from 'puppeteer'; // used locally
-import puppeteerCore from 'puppeteer-core';     // used on production
+import { Browser } from 'puppeteer';
 import chromium from '@sparticuz/chromium';
 
 export async function launchBrowser(): Promise<any> {
   const isProduction = process.env.VERCEL === '1';
 
   if (isProduction) {
-    const executablePath = path.join(
-      process.cwd(),
-      '.next',
-      'server',
-      'bin',
-      'chromium' // name of the binary in the bin folder
-    );
-
-    return await puppeteerCore.launch({
+    const puppeteer = await import('puppeteer-core');
+    return await puppeteer.launch({
       args: chromium.args,
-      executablePath,
+      executablePath: await chromium.executablePath(), // ✅ proper path
       headless: chromium.headless,
     });
   } else {
+    const puppeteer = await import('puppeteer');
     return await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
