707647d5e019797c60d6176623c17e5a7ceb770c
NitzanBen24 <nitzanben24@gmail.com>
2025-05-22 16:08:33 +0300

    V3: fix

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -6,17 +6,15 @@ import { fileURLToPath } from "url";
 // Detect environment
 const isProduction = process.env.VERCEL;
 
-// Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // // Define local Chromium binary path (adjust if needed)
-// const localChromiumPath = path.join("/var/task/public/chromium/chromium");
-const localChromiumPath = path.join(__dirname, "../../../chromium");
+const localChromiumPath = path.join(__dirname, "chromium", "chromium");
 
 export async function launchBrowser(): Promise<Browser> {
   if (isProduction) {
-    const executablePath = path.join(process.cwd(), "chromium", "chromium"); 
+    //const executablePath = path.join(process.cwd(), "chromium", "chromium"); 
 
     return await puppeteer.launch({
       args: chromium.args,
