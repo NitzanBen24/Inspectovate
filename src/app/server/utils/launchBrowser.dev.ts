fcc21e4616efa927e8da908d9299aa6c64971532
NitzanBen24 <nitzanben24@gmail.com>
2025-05-22 22:48:24 +0300

    V3: add remove puppeter for prod

--- src/app/server/utils/launchBrowser.dev.ts
+++ src/app/server/utils/launchBrowser.dev.ts
@@ -1,8 +1,8 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
-export async function launchBrowser() {
-  return puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-}
+// export async function launchBrowser() {
+//   return puppeteer.launch({
+//     headless: true,
+//     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//   });
+// }
