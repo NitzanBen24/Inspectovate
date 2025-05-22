5212fa5c41a1088fabfbaafbe9ab0475b48b12bc
NitzanBen24 <nitzanben24@gmail.com>
2025-05-22 11:41:37 +0300

    V3: change next.config

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,14 +1,23 @@
 import puppeteer, { Browser } from "puppeteer";
 import chromium from "@sparticuz/chromium";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Detect environment
 const isProduction = process.env.VERCEL;
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define local Chromium binary path (adjust if needed)
+const localChromiumPath = path.join(__dirname, "../../../public/chromium-bin/chromium");
 
 export async function launchBrowser(): Promise<Browser> {
   if (isProduction) {
     return await puppeteer.launch({
       args: chromium.args,
-      executablePath: await chromium.executablePath(),
+      executablePath: localChromiumPath,
       headless: chromium.headless,
     });
   } else {
