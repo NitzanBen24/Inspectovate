04ec077e79597e71407271ad54eca5f28afbdcb9
NitzanBen24 <nitzanben24@gmail.com>
2025-05-23 13:05:39 +0300

    V3: change launchBrowser

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -3,19 +3,20 @@ import chromium from '@sparticuz/chromium';
 
 export async function launchBrowser() {
     const isProduction = process.env.VERCEL === '1';
+    console.log('isProduction', isProduction)
 
     if (isProduction) {
         const executablePath = await chromium.executablePath();
-        const puppeteerCore = (await import('puppeteer-core')).default;
 
+        const { default: puppeteerCore } = await import('puppeteer-core');
+        
         return puppeteerCore.launch({
             args: chromium.args,
             executablePath,
             headless: true,
         });
     } else {
-        const puppeteer = (await import('puppeteer')).default;
-
+        const { default: puppeteer } = await import('puppeteer');
         return puppeteer.launch({
             headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
