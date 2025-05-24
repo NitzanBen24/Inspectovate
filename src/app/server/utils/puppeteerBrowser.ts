2441af6cad57e1e1b7e8342fd2de9f86820fd552
NitzanBen24 <nitzanben24@gmail.com>
2025-05-24 19:43:41 +0300

    V3: remove puppeter form production

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,21 +1,11 @@
+import puppeteer from 'puppeteer-core';
 import chromium from '@sparticuz/chromium-min';
 
-
 export async function launchBrowser() {
-    const isProduction = process.env.VERCEL === '1';
-
-    if (isProduction) {
-        const { default: puppeteerCore } = await import('puppeteer-core');        
-        return puppeteerCore.launch({
-            args: chromium.args,
-            executablePath: await chromium.executablePath(),
-            headless: true,
-        });
-    } else {
-        const { default: puppeteer } = await import('puppeteer');
-        return puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
-    }
+    return await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: true,
+      });
 }
