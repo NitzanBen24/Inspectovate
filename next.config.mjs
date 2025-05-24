5cc791acac7753d1291e3a9f656e5b1c33adec83
NitzanBen24 <nitzanben24@gmail.com>
2025-05-24 18:36:29 +0300

    fix branch

--- next.config.mjs
+++ next.config.mjs
@@ -7,17 +7,20 @@ const __dirname = dirname(__filename);
 const nextConfig = {
   reactStrictMode: false,
   webpack(config) {
+    
     config.resolve.alias['@fortawesome/fontawesome-svg-core/styles.css'] =
       '@fortawesome/fontawesome-svg-core';
+    
     return config;
+
   },
-  experimental: {
-    outputFileTracingIncludes: {
-      './src/app/api/generate-pdf/': [
-        resolve(__dirname, 'node_modules', '@sparticuz', 'chromium-min', 'bin', 'chromium.br'),
-      ],
-    },
-  },
+  // experimental: {
+  //   outputFileTracingIncludes: {
+  //     './src/app/api/generate-pdf/': [
+  //       resolve(__dirname, 'node_modules', '@sparticuz', 'chromium-min', 'bin', 'chromium.br'),
+  //     ],
+  //   },
+  // },
 };
 
 export default nextConfig;
