884cc0fefa8c055be8f900097e5f1ee99562773b
NitzanBen24 <nitzanben24@gmail.com>
2025-05-25 01:26:09 +0300

    V3: test missing file to commit

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -6,8 +6,20 @@ import { formatDateRTL } from '@/app/server/utils';
 import { launchBrowser } from '@/app/server/utils/puppeteerBrowser';
 import { appStrings } from '@/app/utils/AppContent';
 import { User } from '@/app/utils/types/entities';
-import { PdfField } from '@/app/utils/types/formTypes';
+import { EmberBlock, PdfField } from '@/app/utils/types/formTypes';
 import { NextRequest, NextResponse } from 'next/server';
+
+import puppeteer from 'puppeteer-core';
+import chromium from '@sparticuz/chromium-min';
+
+export async function _launchBrowser() {
+    return await puppeteer.launch({
+        args: chromium.args,        
+        executablePath: await chromium.executablePath(),
+        headless: true,
+      });
+}
+
 
 const _addUserInfo = async (user: User, fields: PdfField[], companyInfo: any) => {    
     
@@ -37,6 +49,44 @@ const _getfileName = (fields: PdfField[], today: string) => {
     )
 }
 
+
+function _generateHtml({
+    date, 
+    formFields,
+    blocks,
+  }: {
+    date: string;
+    formFields: PdfField[];    
+    blocks: EmberBlock[];
+  }) {
+    return `
+    <html dir="rtl">
+      <head>
+        <meta charset="utf-8" />
+        <style>
+            @page { margin: 40px; }
+            body { font-family: Arial, sans-serif; padding: 0; }
+            
+        </style>
+      </head>
+      <body>
+        <div class="section mt-20">
+                    <p class="bold-underline">לאחר בדיקת התוצאות אשר התקבלו הריני מאשר מתקן זה</p>
+                    <div class="row">
+                        <div class="section">
+                            <span class="text-bold mb-6">בברכה</span>
+                            <span class="text-bold"> ניצן</span>
+                            <span class="text-bold">בודק חשמל מוסמך</span>
+                            <span class="text-bold mb-6"></span>
+                        </div>
+                        
+                    </div>
+                </div>
+      </body>
+    </html>
+    `;
+  }
+
 async function _handleDynamicSend(payload: any) {
 
     try {
@@ -50,9 +100,9 @@ async function _handleDynamicSend(payload: any) {
         const updatedFormFields = await _addUserInfo(payload.user, payload.staticFields, companyInfo);        
         const today = formatDateRTL(new Date()).toString();
         
-        const html = generateHtml({ date: today, formFields: updatedFormFields, blocks: payload.dynamicBlocks });
+        const html = _generateHtml({ date: today, formFields: updatedFormFields, blocks: payload.dynamicBlocks });
 
-        const browser = await launchBrowser();
+        const browser = await _launchBrowser();
           
         try {
             const page = await browser.newPage(); 
@@ -79,6 +129,9 @@ async function _handleDynamicSend(payload: any) {
 
 }
 
+
+
+
 export async function POST(req: NextRequest) {
     
     try {
@@ -89,8 +142,6 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: "Missing payload data!" }, { status: 400 });
         }
 
-
-    
         // const result = await sendDynamicForm(payload);
         const result = await _handleDynamicSend(payload);
         
