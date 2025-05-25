0581a8a822f60e2c49a398de27fbe67d9ac941bd
NitzanBen24 <nitzanben24@gmail.com>
2025-05-25 14:01:29 +0300

    V3: rting again sparticuz/chromium

--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,15 +1,31 @@
 //import chromium from '@sparticuz/chromium-min';
-import chromium from 'chrome-aws-lambda';
-import puppeteerCore from 'puppeteer-core';
+// import chromium from 'chrome-aws-lambda';
+// import puppeteerCore from 'puppeteer-core';
+import puppeteer from 'puppeteer-core';
+import chromium from '@sparticuz/chromium';
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 export async function launchBrowser() {
     console.log('Launching browser!!:')
+
+    const executablePath = isDev
+    ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    : await chromium.executablePath(); // ✅ with parentheses
+
+  console.log('executablePath', executablePath); // 🔍 Add this log
+
+//   return await puppeteer.launch({
+//     args: chromium.args,
+//     defaultViewport: chromium.defaultViewport,
+//     executablePath,
+//     headless: chromium.headless,
+//   });
+
     if (isDev) {
         // Use local Chrome with default args (or minimal args)
         const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-        return puppeteerCore.launch({
+        return puppeteer.launch({
           executablePath,
           headless: false, // for dev, you probably want to see the browser
           // Optionally minimal or no args:
@@ -18,9 +34,9 @@ export async function launchBrowser() {
         });
       } else {
         // Production with chrome-aws-lambda Chromium
-        const executablePath = await chromium.executablePath;
+        const executablePath = await chromium.executablePath();
         console.log('executablePath!!:',executablePath)
-        return puppeteerCore.launch({
+        return puppeteer.launch({
           args: chromium.args,
           defaultViewport: chromium.defaultViewport,
           executablePath,
