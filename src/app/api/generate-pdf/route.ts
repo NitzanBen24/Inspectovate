b7fa03dc9cf676bc2da32278ced4d4c121e36a3f
NitzanBen24 <nitzanben24@gmail.com>
2025-05-28 15:07:33 +0300

    V3: add runtime = nodejs to route

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -1,6 +1,8 @@
 import { sendDynamicForm } from '@/app/server/actions/dynamicFormActions';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'nodejs';
+
 
 export async function POST(req: NextRequest) {
     
--- src/app/server/utils/puppeteerBrowser.ts
+++ src/app/server/utils/puppeteerBrowser.ts
@@ -1,41 +1,35 @@
-//import chromium from '@sparticuz/chromium-min';
-// import chromium from 'chrome-aws-lambda';
-// import puppeteerCore from 'puppeteer-core';
 import puppeteer from 'puppeteer-core';
 import chromium from '@sparticuz/chromium';
-import path from 'path';
-import fs from "fs";
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 export async function launchBrowser() {
     console.log('Launching browser!!:')
-
-	console.log('executablePath!!:',path.join(process.cwd(),'node_modules/@sparticuz/chromium/bin/'))
-	console.log('Chromium path:', await chromium.executablePath());
-
-    const executablePath = isDev
-    ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-    : await chromium.executablePath(); 
-	
-	//: path.join(process.cwd(),'node_modules/@sparticuz/chromium/bin/')
 	
     if (isDev) {
         // Use local Chrome with default args (or minimal args)        
         return puppeteer.launch({
-          executablePath,
-          headless: false, // for dev, you probably want to see the browser
+          executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+          headless: false,
           args: [],
-          defaultViewport: null, // or your preferred viewport
+          defaultViewport: null,
         });
-      } else {
-        // const executablePath = await chromium.executablePath();
-        console.log('executablePath!!:',executablePath)
-        return puppeteer.launch({
-          args: chromium.args,
-          defaultViewport: chromium.defaultViewport,
-          executablePath,
-          headless: chromium.headless,
-        });
-      }
+	} 
+	  
+	// Production: use @sparticuz/chromium
+	const executablePath = await chromium.executablePath();
+	
+	if (!executablePath) {
+		throw new Error("Chromium executablePath is undefined. Deployment may be missing binary.");
+	}
+	
+	console.log('executablePath!!:',executablePath)
+
+	return puppeteer.launch({
+		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
+		executablePath,
+		headless: chromium.headless,
+	});
+      
 }
